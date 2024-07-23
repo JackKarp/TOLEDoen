0b63358c0be5e2cdc44677230243c312aa60c2f0
Jonathan Quitoriano <jon.quitoriano@gmail.com>
2024-07-23 14:33:19 -0700

    add simple canvas pull and envfile

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TOLEDoen
 Creating something with a Transparent OLED
+
+```
+[!Important]
+Make an env file in the canvas folder with "apikey : $YOURAPIKEY"
+```
---
+++ canvas/canvas-api-req-test.py
@@ -0,0 +1,17 @@
+import requests
+
+def read_envfile():
+    dict = {}
+    with open("./canvas/env") as envfile:
+        for line in envfile:
+            stripped_line = "".join(line.split()).split(":")
+            dict[stripped_line[0]] = stripped_line[1] 
+    return dict
+
+
+
+
+envdict = read_envfile()
+print(envdict)
+res = requests.get(f"https://canvas.ubc.ca/api/v1/users/self", headers={"Authorization" : f"Bearer {envdict["apikey"]}"})
+print(res)
