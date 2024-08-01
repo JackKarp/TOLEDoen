fb9f7e6f30839eb0c7bbfee5922a5e9bfefa0904
JackKarp <55057308+JackKarp@users.noreply.github.com>
2024-08-01 13:51:46 -0700

    bouncetime

--- button_please.py
+++ button_please.py
@@ -41,7 +41,7 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use number pin numbering
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(15,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(15,GPIO.RISING,callback=button_callback,bouncetime=300) # Setup event on pin 10 rising edge
 
 serial = spi(port=0, address=0)
 device = ssd1309(serial)
