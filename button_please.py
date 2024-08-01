c786fddec490dff79431aa06796d8eff98f1f4d7
JackKarp <55057308+JackKarp@users.noreply.github.com>
2024-08-01 13:38:14 -0700

    BCM

--- button_please.py
+++ button_please.py
@@ -34,10 +34,10 @@ def get_content():
     return canvas.get_courses()
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setmode(GPIO.BCM) # Use number pin numbering
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(15,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 
 try:
     serial = spi(port=0, address=0)
