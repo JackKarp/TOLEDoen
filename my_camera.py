b90e76d9bd341077421147973eecec086947a39a
JackKarp <55057308+JackKarp@users.noreply.github.com>
2024-08-03 21:55:20 -0700

    no stop

--- my_camera.py
+++ my_camera.py
@@ -3,5 +3,5 @@ from picamera2 import Picamera2, Preview
 cam = Picamera2()
 cam.start_preview(Preview.QT)
 
-input("enter to end preview")
-cam.stop_preview()
+# input("enter to end preview")
+# cam.stop_preview()
