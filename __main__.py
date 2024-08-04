c0766be629259884dcedcbc4d90b90e12a173889
Serrindipity <jon.quitoriano@gmail.com>
2024-08-03 23:12:26 -0700

    :confused:

--- __main__.py
+++ __main__.py
@@ -53,4 +53,5 @@ d = init_pins(button_pin)
 pm = PageMachine(pages, device=d)
 GPIO.add_event_detect(button_pin,GPIO.RISING,callback=clean_cycle,bouncetime=50) # Setup event on pin 10 rising edge
 while True:
-    pm.cycle()
+    if not pm.current_state.flag:
+        pm.cycle()
