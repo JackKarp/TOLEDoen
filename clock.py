da31719ff0caa19affbdc68af80539830a8a5ff6
Serrindipity <jon.quitoriano@gmail.com>
2024-08-03 23:21:16 -0700

    remove all flags and make clock enter func render

--- clock.py
+++ clock.py
@@ -16,47 +16,49 @@ def posn(angle, arm_length):
     dy = int(math.sin(math.radians(angle)) * arm_length)
     return (dx, dy)
 
+def render_clock(device):
+    now_gmt = datetime.datetime.now()
+    now = now_gmt.astimezone(tz)
+    today_date = now.strftime("%d %b %y")
+    today_time = now.strftime("%I:%M:%S %P")
+    if today_time != today_last_time:
+        today_last_time = today_time
+        with canvas(device) as draw:
+            now_gmt = datetime.datetime.now()
+            now = now_gmt.astimezone(tz)
+            today_date = now.strftime("%d %b %y")
+
+            margin = 8
+
+            cx = 30
+            cy = min(device.height, 64) / 2
+
+            left = cx - cy
+            right = cx + cy
+
+            hrs_angle = 270 + (30 * (now.hour + (now.minute / 60.0)))
+            hrs = posn(hrs_angle, cy - margin - 7)
+
+            min_angle = 270 + (6 * now.minute)
+            mins = posn(min_angle, cy - margin - 2)
+
+            sec_angle = 270 + (6 * now.second)
+            secs = posn(sec_angle, cy - margin - 2)
+
+            draw.ellipse((left + margin, margin, right - margin, min(device.height, 64) - margin), outline="white")
+            draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
+            draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill="white")
+            draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill="red")
+            draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2), fill="white", outline="white")
+            draw.text((2 * (cx + margin), cy - 8), today_date, fill="yellow")
+            draw.text((2 * (cx + margin), cy), today_time, fill="yellow")
+
 
 def run_clock(device, page):
     today_last_time = "Unknown"
     tz = pytz.timezone("America/Los_Angeles")
     while page.flag[0]:
-        now_gmt = datetime.datetime.now()
-        now = now_gmt.astimezone(tz)
-        today_date = now.strftime("%d %b %y")
-        today_time = now.strftime("%I:%M:%S %P")
-        if today_time != today_last_time:
-            today_last_time = today_time
-            with canvas(device) as draw:
-                now_gmt = datetime.datetime.now()
-                now = now_gmt.astimezone(tz)
-                today_date = now.strftime("%d %b %y")
-
-                margin = 8
-
-                cx = 30
-                cy = min(device.height, 64) / 2
-
-                left = cx - cy
-                right = cx + cy
-
-                hrs_angle = 270 + (30 * (now.hour + (now.minute / 60.0)))
-                hrs = posn(hrs_angle, cy - margin - 7)
-
-                min_angle = 270 + (6 * now.minute)
-                mins = posn(min_angle, cy - margin - 2)
-
-                sec_angle = 270 + (6 * now.second)
-                secs = posn(sec_angle, cy - margin - 2)
-
-                draw.ellipse((left + margin, margin, right - margin, min(device.height, 64) - margin), outline="white")
-                draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
-                draw.line((cx, cy, cx + mins[0], cy + mins[1]), fill="white")
-                draw.line((cx, cy, cx + secs[0], cy + secs[1]), fill="red")
-                draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2), fill="white", outline="white")
-                draw.text((2 * (cx + margin), cy - 8), today_date, fill="yellow")
-                draw.text((2 * (cx + margin), cy), today_time, fill="yellow")
-
+        render_clock(device)
         time.sleep(0.1)
 
 
