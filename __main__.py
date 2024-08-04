64326dab59cb5a4ed5ec9990510ab24bcf883cf9
Serrindipity <jon.quitoriano@gmail.com>
2024-08-03 22:15:40 -0700

    re-add cycle to end of main

--- __main__.py
+++ __main__.py
@@ -53,4 +53,4 @@ pages = make_page_list()
 d = init_pins(button_pin)
 pm = PageMachine(pages, device=d)
 GPIO.add_event_detect(button_pin,GPIO.RISING,callback=clean_cycle,bouncetime=300) # Setup event on pin 10 rising edge
-
+pm.cycle()
