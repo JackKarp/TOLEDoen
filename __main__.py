a117b9a95d9d7e7013b0b691ef496b446b3bc36f
Serrindipity <jon.quitoriano@gmail.com>
2024-08-03 23:10:38 -0700

    this is hecked

--- __main__.py
+++ __main__.py
@@ -52,4 +52,5 @@ pages = make_page_list()
 d = init_pins(button_pin)
 pm = PageMachine(pages, device=d)
 GPIO.add_event_detect(button_pin,GPIO.RISING,callback=clean_cycle,bouncetime=50) # Setup event on pin 10 rising edge
-pm.cycle()
+while True:
+    pm.cycle()
