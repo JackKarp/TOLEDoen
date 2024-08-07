55e0572013227c14080c9e980ff14f192663938e
Serrindipity <jon.quitoriano@gmail.com>
2024-08-07 16:42:12 -0700

    test wrap

--- test_sideways_render.py
+++ test_sideways_render.py
@@ -3,10 +3,14 @@ from luma.core.render import canvas
 from luma.oled.device import ssd1309
 from PIL import Image, ImageDraw
 
+import textwrap
+
 serial = spi(port=0, address=0)
 device = ssd1309(serial, rotate=3)
 
+"TODO: Figure out programatically how to wrap"
 with canvas(device) as d:
-    d.multiline_text((0,0),u"testing \n this thing",fill="white")
-
-input()
+    xy = (device.width, device.height)
+    text = "testing this thingymajigy"
+    wrapped = textwrap.wrap(text, width=11)
+    d.multiline_text((0,0),'\n'.join(wrapped),fill="white")
