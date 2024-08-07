d4103110c5165de25e49b95b394e384afa6a510f
Serrindipity <jon.quitoriano@gmail.com>
2024-08-07 16:20:14 -0700

    there's literally a rotate param in ssd1309() :silly:

--- test_sideways_render.py
+++ test_sideways_render.py
@@ -4,9 +4,9 @@ from luma.oled.device import ssd1309
 from PIL import Image, ImageDraw
 
 serial = spi(port=0, address=0)
-device = ssd1309(serial)
-img = Image.new('L', (120, 50))
-img.rotate(90)
-device.display(img)
+device = ssd1309(serial, rotate=90)
+
+with canvas(device) as d:
+    d.text((0,0),"testing this thing",fill="white")
 
 input()
