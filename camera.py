e1790bfeef02be3d286c7413574e33cb2a804113
JackKarp <55057308+JackKarp@users.noreply.github.com>
2024-08-30 22:11:15 -0700

    added heigh functionality

--- camera.py
+++ camera.py
@@ -86,8 +86,7 @@ with handsModule.Hands(static_image_mode=False, min_detection_confidence=0.7, mi
                         pos_dict[point] = pixelCoordinatesLandmark
         
         if(pos_dict[0] and pos_dict[8] and pos_dict[12] and pos_dict[16] and pos_dict[20]):
-            height = abs(pos_dict[12][1] - pos_dict[0][1])
-
+            height = max(abs(pos_dict[12][1] - pos_dict[0][1]),abs(pos_dict[8][1] - pos_dict[0][1]),abs(pos_dict[16][1] - pos_dict[0][1]),abs(pos_dict[20][1] - pos_dict[0][1]))
             if(abs(pos_dict[8][1] - pos_dict[0][1]) > height/2 and abs(pos_dict[12][1] - pos_dict[0][1]) > height/2 and abs(pos_dict[20][1] - pos_dict[0][1]) < height/2 and abs(pos_dict[16][1] - pos_dict[0][1]) < height/2 and abs(pos_dict[8][0] - pos_dict[12][0]) < 20):
                 print("clk")
                 pm.go_to(1) #go to clock
