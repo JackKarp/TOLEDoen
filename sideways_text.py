06662b3d80e5bc058afe189484ac1f56f4731a71
JackKarp <55057308+JackKarp@users.noreply.github.com>
2024-09-03 13:33:24 -0700

    better text wrapping

--- sideways_text.py
+++ sideways_text.py
@@ -1,20 +1,20 @@
 from luma.core.render import canvas
 
 def wrapped_text(string):
-    words = string.split(' ')
     new_words = []
-    for i, word in enumerate(words):
-        if len(word) > 10:
-            if len(word) >= 20:
-                new_words.append(word[0:12])
-                # offset += 10
-                new_words.append(word[12:24])
-                # offset += 10
-                new_words.append(word[24:len(word)])
-            else:
-                new_words.append(word[0:12])
-                # offset += 10
-                new_words.append(word[12:len(word)])
-        else:
-            new_words.append(word)
+    for i in range(0, len(string), 10):
+        new_words.append(string[i:i+10])
+    # if len(string) > 10:
+    #     if len(word) >= 20:
+    #         new_words.append(word[0:12])
+    #         # offset += 10
+    #         new_words.append(word[12:24])
+    #         # offset += 10
+    #         new_words.append(word[24:len(word)])
+    #     else:
+    #         new_words.append(word[0:12])
+    #         # offset += 10
+    #         new_words.append(word[12:len(word)])
+    # else:
+    #     new_words.append(word)
     return "\n".join(new_words) #, offset
