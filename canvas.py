53820da55fabd7ecd0bd548c23cd495ddca6d589
JackKarp <55057308+JackKarp@users.noreply.github.com>
2024-09-03 13:05:17 -0700

    fixed canvas

--- canvas.py
+++ canvas.py
@@ -22,7 +22,7 @@ def get_content():
 
 def run_canvas(device):
     content = get_content()
-    lines = wrapped_text(content[1].name, device)
+    lines = wrapped_text(content[1].name)
     with canvas(device) as draw:
         for i, line in enumerate(lines):
             draw.text((3, i*10),line)
